21c3ee97b1d8af04833c13f5dbb0428581132a30
Zach Pace <zpace21@gmail.com>
2016-06-03 18:07:30 -0500

    MaNGA_SkyView implemented, nonfunctional because of astroquery bug

--- imaging_tools.py
+++ imaging_tools.py
@@ -1,16 +1,25 @@
 import numpy as np
 
-from astroquery.skyview import SkyView
+from astropy import table as t, constants as c, units as u, \
+    coordinates as coords
+from astroquery.sdss import SDSSClass
 
-class MaNGA_SkyView(SkyView):
+class MaNGA_SkyView(SDSSClass):
     '''
     subclass of SkyView, which allows access to FITS thumbnails of
         a MaNGA object
     '''
-    @classmethod
-    def from_drpall_row(cls, row):
-        raise NotImplementedError
 
-    @classmethod
-    def from_wcs_header(cls, header):
-        raise NotImplementedError
+    def images_from_drpall_row(self, row):
+        p = '{}d {}d'.format(row['ifura'], row['ifudec'])
+        p = coords.SkyCoord(p, frame='icrs')
+        bands = ['u', 'g', 'r', 'i', 'z']
+        return self.get_images(
+            coordinates=p, band=bands, radius=20.*u.arcsec)
+
+    def images_from_wcs_header(self, header):
+        p = '{}d {}d'.format(header['CRVAL1'], header['CRVAL2'])
+        p = coords.SkyCoord(p, frame='icrs')
+        bands = ['u', 'g', 'r', 'i', 'z']
+        return self.get_images(
+            coordinates=p, band=bands, radius=20.*u.arcsec)
