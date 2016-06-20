8f0b4519465dcdcf6bbb333f3494d0cb50330358
Zach Pace <zpace21@gmail.com>
2016-06-19 20:25:39 -0500

    MaNGA_ImageDownoader rolled back to nothing, due to API conflict--to fix

--- imaging_tools.py
+++ imaging_tools.py
@@ -4,22 +4,8 @@ from astropy import table as t, constants as c, units as u, \
     coordinates as coords
 from astroquery.sdss import SDSSClass
 
-class MaNGA_SkyView(SDSSClass):
+class MaNGA_ImageDownloader(SDSSClass):
     '''
     subclass of SkyView, which allows access to FITS thumbnails of
         a MaNGA object
     '''
-
-    def images_from_drpall_row(self, row):
-        p = '{}d {}d'.format(row['ifura'], row['ifudec'])
-        p = coords.SkyCoord(p, frame='icrs')
-        bands = ['u', 'g', 'r', 'i', 'z']
-        return self.get_images(
-            coordinates=p, band=bands, radius=20.*u.arcsec)
-
-    def images_from_wcs_header(self, header):
-        p = '{}d {}d'.format(header['CRVAL1'], header['CRVAL2'])
-        p = coords.SkyCoord(p, frame='icrs')
-        bands = ['u', 'g', 'r', 'i', 'z']
-        return self.get_images(
-            coordinates=p, band=bands, radius=20.*u.arcsec)
