8c93b5b845147f00f09dcce9240f00656d630238
zpace <zpace21@gmail.com>
2016-09-21 12:10:26 -0500

    update to broadband lum

--- spectrophot.py
+++ spectrophot.py
@@ -1,14 +1,21 @@
 import numpy as np
 
 from scipy.interpolate import interp1d
-from scipy.integrate import quad
+from scipy.integrate import trapz
 
-from astropy import units as u, table as t
+from astropy import units as u, constants as c, table as t
 
 
-def spec2phot_source(lam, Llam, band):
+l_eff_d = {'r': 6166. * u.AA, 'i': 7480. * u.AA, 'z': 8932. * u.AA}
+l_wid_d = {'r': 550. * u.AA, 'i': 1300. * u.AA, 'z': 1000. * u.AA}
+
+
+def lumdens2bbdlum(lam, Llam, band):
     '''
-    convolve source spectrum with a filter
+    Convert a spectral luminosity density to a broadband luminosity
+
+    Convolve source spectrum/a with a filter; specific routine derived '
+        from MaNGA's ml_mangatosdssimage.pro routine from DRP
 
     Parameters
     ----------
@@ -22,38 +29,25 @@ def spec2phot_source(lam, Llam, band):
         chosen band
     '''
 
-    # convert to per unit frequency, with a hack to get around Ldens
-    Lnu = (Llam / u.cm**2.).to('Lsun Hz-1 cm-2',
-                               equivalencies=u.spectral_density(
-                                   wav=lam)) * u.cm**2.
-    Lnu = Lnu.to('Lsun Hz-1')
+    # make sure everything's in the right units
+    Llam = Llam.to('erg s-1 AA-1')
+    lam = lam.to('AA')
     nu = lam.to('Hz', equivalencies=u.spectral())
-
-    # set up interpolator
-    Lnu_interp = interp1d(x=nu.value, y=Lnu.value, fill_value=0.,
-                          bounds_error=False)
 
     # read in filter table
     band_tab = t.Table.read('filters/{}_SDSS.res'.format(band),
                             names=['lam', 'f'], format='ascii')
-    # convert to per unit frequency
-    band_tab['lam'].unit = u.AA
-    band_tab['nu'] = band_tab['lam'].to('Hz', equivalencies=u.spectral())
 
     # set up interpolator
-    band_interp = interp1d(x=band_tab['nu'].quantity.value,
+    band_interp = interp1d(x=band_tab['lam'].quantity.value,
                            y=band_tab['f'], fill_value=0.,
                            bounds_error=False)
 
-    # limits of integration
-    nu_ulim = (1000. * u.AA).to('Hz', equivalencies=u.spectral()).value
-    nu_llim = (15000. * u.AA).to('Hz', equivalencies=u.spectral()).value
+    f = band_interp(lam)
 
-    def bandpassify(freq):
-        Lnu_ = Lnu_interp(freq)
-        f_ = band_interp(freq)
-        return (Lnu_ * f_)
+    # convert to Lnu, by multiplying by lam^2/c
+    Lnu = (Llam * lam**2. / c.c).to('Lsun Hz-1')
 
-    L, dL = quad(bandpassify, nu_llim, nu_ulim)
+    L = trapz(x=nu.value[::-1], y=f * Lnu.value, axis=-1) * u.Lsun
 
     return L
