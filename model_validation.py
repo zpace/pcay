0ffe465292688013bb33a099b593c988dd96d30d
zpace <zpace21@gmail.com>
2016-10-18 16:58:43 -0500

    specify narrow D4000 index where appropriate

--- model_validation.py
+++ model_validation.py
@@ -30,15 +30,18 @@ class ModelDataCompare(object):
     """
     Compare the model set and the data in a bunch of spaces
     """
-    def __init__(self, pca, dereds, rimgs, rimg_thr=0.):
+    def __init__(self, pca, dereds, rimgs, Hd_em_EWs, rimg_thr=0.):
         self.pca = pca
         self.dereds = dereds
         self.rimgs = rimgs
 
+        self.Hd_em_EWs = np.concatenate(
+            [h.flatten() for h in Hd_em_EWs])
+
         self.mask = np.concatenate(
             [(r <= rimg_thr).flatten() for r in rimgs])
 
-        for p in ['Hdelta_A', 'D4000']:
+        for p in ['Hdelta_A', 'Dn4000']:
             # retrieve appropriate function from spec_tools
             f = getattr(spec_tools, '{}_index'.format(p))
 
@@ -55,13 +58,14 @@ class ModelDataCompare(object):
         fig = plt.figure(figsize=(4, 4), dpi=300)
         ax = fig.add_subplot(111)
 
-        data = np.column_stack([np.concatenate(self.D4000),
-                                np.concatenate(self.Hdelta_A)])
+        data = np.column_stack(
+            [np.concatenate(self.Dn4000),
+             np.concatenate(self.Hdelta_A) + self.Hd_em_EWs])
         data = data[~self.mask]
 
         KDE_data = KDEMultivariate(data=data, var_type='cc')
 
-        KDE_models = KDEMultivariate(data=[self.pca.metadata['D4000'],
+        KDE_models = KDEMultivariate(data=[self.pca.metadata['Dn4000'],
                                            self.pca.metadata['Hdelta_A']],
                                      var_type='cc')
 
@@ -86,8 +90,8 @@ class ModelDataCompare(object):
         ax.set_ylim([-6., 12.])
         ax.set_xlim([1., 2.5])
 
-        ax.set_xlabel('D4000')
-        ax.set_ylabel(r'H$\delta_{A}$')
+        ax.set_xlabel(self.pca.metadata['Dn4000'].meta['TeX'])
+        ax.set_ylabel(self.pca.metadata['Hdelta_A'].meta['TeX'])
 
         plt.tight_layout()
 
@@ -104,14 +108,21 @@ if __name__ == '__main__':
 
     gals = ['8566-12705', '8567-12701', '8939-12704', '8083-12704',
             '8134-12702', '8134-9102', '8135-12701', '8137-12703',
-            '8140-12703', '8140-12701', '8140-3701', '8243-12704']
+            '8140-12703', '8140-12701', '8140-3701', '8243-12704',
+            '8244-9101', '8247-9101', '8249-12703', '8249-12704',
+            '8252-12705', '8252-6102', '8253-6104', '8254-3704', '8254-9101',
+            '8257-12701', '8257-6101', '8257-6103', '8258-12704']
+
+    #gals = ['8567-12701']
 
     p_i = [g.split('-') for g in gals]
 
     dereds = [MaNGA_deredshift.from_plateifu(
         plate=int(p), ifu=int(i), MPL_v=mpl_v) for (p, i) in p_i]
     rimgs = [d.drp_hdulist['RIMG'].data for d in dereds]
+    Hdelt_em_EWs = [d.dap_hdulist['EMLINE_SEW'].data[14, ...] for d in dereds]
 
-    MDComp = ModelDataCompare(pca=pca, dereds=dereds, rimgs=rimgs)
+    MDComp = ModelDataCompare(pca=pca, dereds=dereds, rimgs=rimgs,
+                              Hd_em_EWs=Hdelt_em_EWs)
     mdcomp_fig = MDComp.D4000_Hd_fig()
     mdcomp_fig.savefig('D4000-HdA.png', dpi=300)
