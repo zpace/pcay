3e7b947a8d7469b4752d567bb258fe67d36e65c7
zpace <zpace21@gmail.com>
2016-12-01 15:49:19 -0600

    minor doc updates

--- radial.py
+++ radial.py
@@ -4,7 +4,7 @@ from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.gaussian_process.kernels import RBF, ConstantKernel as Const
 
 
-def radial_gp(r, q, q_unc, q_bdy=[-np.inf, np.inf]):
+def radial_gp(r, q, q_unc, scale, q_bdy=[-np.inf, np.inf]):
     '''
     compute radial gaussian process for regressing some quantity against Re
 
@@ -29,12 +29,16 @@ def radial_gp(r, q, q_unc, q_bdy=[-np.inf, np.inf]):
     r = np.atleast_2d(r).T
 
     # build a very flexible kernel: can nominally handle factor
-    # of 20 variation in 1/4 Re, but mmay not need to
-    kernel = Const(5., (1.0e-2, 20.)) * RBF(.25, (.05, 2.))
-
-    nugget = (q_unc / q)**2.
-    # this is bad and I should feel bad
-    nugget = np.maximum(nugget, .05 * np.ones_like(nugget))
+    # of 20 variation in 1/4 Re, but may not need to
+    if scale == 'linear':
+        kernel = Const(5., (1.0e-2, 20.)) * RBF(.25, (.05, 2.))
+        nugget = (q_unc / q)**2.
+    elif scale == 'log':
+        kernel = (Const(np.log10(5.), (-2, np.log10(20.))) *
+                  RBF(.25, (.05, 2.)))
+        nugget = 10.**(2. * (q_unc - q))
+    else:
+        raise ValueError('invalid scale!')
 
     gp = GaussianProcessRegressor(kernel=kernel, alpha=nugget,
                                   n_restarts_optimizer=10)
