3c2ebadc5aac5eb63233601e2f19b67a72d79e33
zpace <zpace21@gmail.com>
2017-02-28 14:03:50 -0600

    covariance from reobserved BOSS

--- cov_obs.py
+++ cov_obs.py
@@ -8,6 +8,8 @@ from astropy.io import fits
 import os
 import sys
 
+from glob import glob
+
 # add manga RC location to path, and import config
 if os.environ['MANGA_CONFIG_LOC'] not in sys.path:
     sys.path.append(os.environ['MANGA_CONFIG_LOC'])
@@ -26,8 +28,6 @@ eps = np.finfo(float).eps
 
 print('MaNGA data-product info:', mpl_v, '({})'.format(m.MPL_versions[mpl_v]))
 print('MaNGA data location:', mangarc.manga_data_loc[mpl_v])
-
-boss_calib_dir = os.path.join(mangarc.zpace_sdss_data_loc, 'boss/calib')
 
 
 class Cov_Obs(object):
@@ -50,65 +50,83 @@ class Cov_Obs(object):
     # =====
 
     @classmethod
-    def from_spAll(cls, spAll, lllim=3650.059970708618, nspec=4378,
-                   dlogl=1.0e-4):
+    def from_BOSS_reobs(cls, spAll, dlogl=1.0e-4, n=None):
         '''
         returns a covariance object made from an spAll file
         '''
 
-        # dict of multiply-observed objects
-        mults, SB_r_mean = Cov_Obs._mults(spAll)
-        del spAll  # clean up!
+        # prepare catalog
+        colnames = ['PLATE', 'MJD', 'FIBERID', 'OBJID']
+        cols = [spAll[1].data[k] for k in colnames]
+        obs = t.Table(cols, names=colnames)
+        obs = obs[obs['OBJID'] != ' ' * 19]
+        objs = obs.group_by('OBJID')
 
-        stack = [
-            Cov_Obs.load_zeronormed_obj_spec(
-                *Cov_Obs.download_obj_specs(obj),
-                lllim=lllim, nspec=nspec, i=i)
-            for i, (k, obj) in enumerate(mults.items())]
+        def morethan1_(tab):
+            return len(tab) > 1
 
-        resids = np.concatenate([s[:len(s) // 2] for s in stack], axis=0)
-        ivars = np.concatenate([s[len(s) // 2:] for s in stack], axis=0)
+        morethan1 =  np.array(list(map(morethan1_, objs.groups)))
 
-        # filter out bad rows
-        bad_rows = (np.isnan(resids).sum(axis=1) > 0)
-        resids = resids[~bad_rows, :]
-        ivars = ivars[~bad_rows, :]
-        nobj, nspec = resids.shape
+        # grouped filter & final grouping
+        objs_dupl = (objs.groups[morethan1])
+        objs_dupl.group_by('OBJID')
+        objids = objs_dupl.groups.keys
+        n_unique = len(objids)
 
-        qw = resids * ivars
-        cov = qw.T.dot(qw) / ivars.T.dot(ivars)
+        # if unspecified, use all galaxies
+        if n is None:
+            n = n_unique
+
+        groups = [tab for tab in objs_dupl.groups]
+        use = [True, ] * n + [False, ] * (n_unique - n)
+        groups = [g for g, u in zip(groups, use) if u]
+
+        # multiply-observed objects
+        diffs, w, good = zip(
+            *[Cov_Obs.process_BOSS_mult(grp, MPL_v, nl)
+              for grp in groups])
+
+        ndiffs = sum(list(map(len, diffs)))
+        logl0 = min(list(map(min, logl)))
+        loglf = max(list(map(max, logl)))
+
+        # full wavelength grid for all objects
+        logl_full = np.arange(logl0, loglf + dlogl, step=dlogl)
+
+        # place differences and weights on common grid
+        diffs_regr, w = zip(put_on_grid(logl_full, logl_sub, A))
+
+        # compute covariance
+        wdiffs = (w * diffs)
+        cov = wdiffs.T.dot(wdiffs) / (w.T.dot(w) - 1.)
+        cov /= np.median(np.diag(cov))
 
         return cls(cov, lllim=lllim, dlogl=dlogl, nobj=nobj)
 
     @classmethod
-    def from_MaNGA_reobs(cls, lllim=3650.059970708618, nspec=4563,
-                         dlogl=1.0e-4, MPL_v=mpl_v, n=None):
+    def from_MaNGA_reobs(cls, dlogl=1.0e-4, MPL_v=mpl_v, n=None):
         '''
         returns a covariance object made from reobserved MaNGA IFU LOGCUBEs
         '''
 
-        drpall = t.Table.read(os.path.join(
-            mangarc.manga_data_loc[mpl_v],
-            'drpall-{}.fits'.format(m.MPL_versions[mpl_v])))
+        drpall = m.load_drpall(mpl_v)
 
         drpall = drpall[drpall['ifudesignsize'] != -9999]
         objs = drpall.group_by('mangaid')
 
-        start = np.array(objs.groups.indices[:-1])
-        stop = np.array(objs.groups.indices[1:])
-
-        repeat = stop - start > 1
-
         # only use objects with multiple observations with same IFU size
         def onesize_(tab):
             return len(np.unique(tab['ifudesignsize'])) == 1
+
+        def morethan1_(tab):
+            return len(tab) > 1
+
         onesize = np.array(list(map(onesize_, objs.groups)))
+        morethan1 = np.array(list(map(morethan1_, objs.groups)))
 
-        # grouped filter
-        obs_dupl = objs.groups[repeat & onesize]
-
-        # final grouping
-        objs_dupl = obs_dupl.group_by('mangaid')
+        # grouped filter & final grouping
+        objs_dupl = (objs.groups[morethan1 & onesize])
+        objs_dupl.group_by('mangaid')
         mangaids = objs_dupl.groups.keys
         n_unique = len(mangaids)
 
@@ -120,16 +138,24 @@ class Cov_Obs(object):
         use = [True, ] * n + [False, ] * (n_unique - n)
         groups = [g for g, u in zip(groups, use) if u]
 
+        # figure out lam low lim and nlam
+        lam = m.load_drp_logcube('8083', '12704', MPL_v)['WAVE'].data
+        lllim = lam.min()
+        nl = len(lam)
+
         # process multiply-observed datacubes, up to limit set
-        diffs, good, n = zip(
-            *[Cov_Obs.process_MaNGA_mult(grp, nspec)
+        diffs, w, good = zip(
+            *[Cov_Obs.process_MaNGA_mult(grp, MPL_v, nl)
               for grp in groups])
 
+        # stack all the differences
         diffs = np.row_stack([p for (p, g) in zip(diffs, good) if g])
-        n = [nn for (nn, g) in zip(n, good) if g]
-        N = np.sum(n)
+        w = np.row_stack([p for (p, g) in zip(w, good) if g])
 
-        cov = np.cov(diffs.T)
+        N = w.shape[0]
+
+        wdiffs = (w * diffs)
+        cov = wdiffs.T.dot(wdiffs) / (w.T.dot(w) - 1.)
         cov /= np.median(np.diag(cov))
 
         return cls(cov, lllim=lllim, dlogl=dlogl, nobj=N)
@@ -170,7 +196,7 @@ class Cov_Obs(object):
 
     def make_im(self, kind):
         l = self.l
-        fig = plt.figure(figsize=(6, 6), dpi=300)
+        fig = plt.figure(figsize=(6, 5), dpi=300)
         ax = fig.add_subplot(111)
 
         vmax = np.abs(self.cov).max()**0.3
@@ -179,8 +205,8 @@ class Cov_Obs(object):
             np.sign(self.cov) * (np.abs(self.cov))**0.3,
             extent=[l.min(), l.max(), l.min(), l.max()],
             vmax=vmax, vmin=-vmax, interpolation='None', aspect='equal')
-        ax.set_xlabel(r'$\lambda$', size=8)
-        ax.set_ylabel(r'$\lambda$', size=8)
+        ax.set_xlabel(r'$\lambda [\AA]$', size=10)
+        ax.set_ylabel(r'$\lambda [\AA]$', size=10)
         cb = plt.colorbar(im, ax=ax, shrink=0.8)
         cb.set_label(r'$\textrm{sign}(K) ~ |K|^{0.3}$')
         plt.savefig('cov_obs_{}.png'.format(kind), dpi=300)
@@ -203,7 +229,7 @@ class Cov_Obs(object):
     # =====
 
     @staticmethod
-    def process_MaNGA_mult(tab, nspec=4563):
+    def process_MaNGA_mult(tab, MPL_v, nspec=4563):
         '''
         check for presence of datacubes for a single multiply-observed
             MaNGA object, and (if the data exists) process the observations
@@ -215,13 +241,13 @@ class Cov_Obs(object):
             being returned
 
         Returns:
-         - resids_normed: SNR-1-normed variance (or zeros array of right shape)
+         - diffs: differences between pairs of spectra of
+             nominally same location (presumed normed to zero)
+         - wt: weights on diffs
          - good: indicates whether resids_normed is good
         '''
 
         from itertools import combinations as comb
-        from scipy.signal import medfilt
-        from astropy.stats import sigma_clip
 
         # load all LOGCUBES for a given MaNGA-ID
         # files have form 'manga-<PLATE>-<IFU>-LOGCUBE.fits.gz'
@@ -232,205 +258,132 @@ class Cov_Obs(object):
                   for plate, ifu in zip(tab['plate'], tab['ifudsgn'])]
 
         # otherwise, move forward and load all files
-        logcubes = [fits.open(f) for f in fnames]
+        logcubes = [m.load_drp_logcube(str(p), i, MPL_v)
+                    for p, i in zip(tab['plate'], tab['ifudsgn'])]
 
-        # handle the odd case where there aren't enough observations loaded
-        n_reobs = len(logcubes)
         naxis1, naxis2, naxis3 = logcubes[0]['FLUX'].data.shape
-        if n_reobs < 2:
-            return np.zeros((0, nspec)), False, 0
 
         # handle the case where LOGCUBEs have different shapes
         if len(set(map(lambda h: h['FLUX'].data.shape, logcubes))) > 1:
-            return np.zeros((0, nspec)), False, 0
+            dummy = np.zeros((0, nspec))
+            return dummy, dummy, False
 
         # extract & reshape data
+        # axis 0: observation; axis 1: spaxel; axis 2: wavelength
         fluxs = np.stack([cube['FLUX'].data.reshape((naxis1, -1)).T
                           for cube in logcubes])
         ivars = np.stack([cube['IVAR'].data.reshape((naxis1, -1)).T
                           for cube in logcubes])
-
-        # replace bad pixels by median of surrounding 300 pixels
-        meds = medfilt(fluxs, [1, 301, 1])
-        fluxs[ivars == 0] = meds[ivars == 0]
-
-        # load rimg extension of only one observation
-        rimg = logcubes[0]['RIMG'].data.flatten()
-        rimg /= rimg.max()
-
-        # exclude rows where any observation has zero total weight
-        # and exclude rows with low r-band signal (< 1% of max)
-        good = np.all(ivars.sum(axis=-1) != 0, axis=0)
-        good *= (rimg > .01)
-        ivars = ivars[:, good, :]
-        fluxs = fluxs[:, good, :]
-
-        # norm the spectra and their uncertainties
-        norms = np.average(fluxs, weights=ivars, axis=2)
-        specs = (fluxs / norms[..., None]).reshape((n_reobs, fluxs.shape[1], -1))
-        uncs_f = (np.sqrt(ivars) * specs).reshape((n_reobs, fluxs.shape[1], -1))
+        mb_mask = np.stack([m.mask_from_maskbits(
+                                a=cube['MASK'].data.reshape((naxis1, -1)).T,
+                                b=[0, 1, 2, 3, 10])
+                            for cube in logcubes])
+        ivars *= (~ mb_mask)
+        ivars[ivars == 0.] = eps
 
         # differences between pairs
-        pairs_ixs = comb(range(n_reobs), 2)
-        diffs = np.row_stack([specs[i1, ...] - specs[i2, ...]
-                          for (i1, i2) in pairs_ixs])
+        n_reobs = len(logcubes)
+        pairs1 = comb(range(n_reobs), 2)
+        pairs2 = comb(range(n_reobs), 2)
+        w = 1. / np.row_stack([1. / (1. / ivars[i1, ...] + 1. / ivars[i2, ...])
+                              for i1, i2 in pairs1])
+        diffs = np.row_stack([fluxs[i1, ...] - fluxs[i2, ...]
+                              for (i1, i2) in pairs2])
 
-        # iterative sigma-clipping
-        diffs_ = sigma_clip(diffs, sigma=3, iters=10, axis=0)
-        bad = np.any(diffs_.mask, axis=1)
-        diffs = diffs[~bad]
-        print(diffs.shape[0])
-
-        return diffs, True, diffs.shape[0]
+        return diffs, w, True
 
     @staticmethod
-    def _mults(spAll, i_lim=10):
+    def process_BOSS_mult(tab, dlogl=1.0e-4):
         '''
-        return a dict of duplicate observations of the same object, using
-            astropy table grouping
-
-        also return a mean object surface brightness (nMgy/arcsec2) to aid
-            in scaling the covariance matrix against MaNGA spaxels
+        return differences between reobservations of same BOSS object
         '''
 
-        (objid, plate, mjd, fiberid) = (
-            spAll[1].data['OBJID'], spAll[1].data['PLATE'],
-            spAll[1].data['MJD'], spAll[1].data['FIBERID'])
-        # average surface brightness within .67 arcsec of object center
-        # (from photometric pipeline)
-        SB_r = t.Column(
-            data=spAll[1].data['APERFLUX'][:, 2, 1] / (np.pi * 0.67**2.),
-            name='SB_r')
-        obs = t.Table([objid, plate, mjd, fiberid, SB_r],
-                      names=['objid', 'plate', 'mjd', 'fiberid', 'SB_r'])
-        obs = obs[obs['objid'] != '                   ']
-        obs = obs[np.nonzero(obs['SB_r'])]
-        obs['objid'] = obs['objid'].astype(int)
-        objs = obs.group_by('objid')
+        from itertools import combinations as comb
 
-        start = np.array(objs.groups.indices[:-1])
-        stop = np.array(objs.groups.indices[1:])
-        # use objects with more than two observations
-        repeat = stop - start > 2
-        obs_dupl = objs.groups[repeat]  # duplicate OBSERVATIONS
-        objs_dupl = obs_dupl.group_by('objid')  # the OBJECTS corresponding
-        objids = list(objs_dupl.groups.keys['objid'])
+        # load all the files
+        baseloc = mangarc.BOSS_data_loc
+        hdulists = load_BOSS_obj(tab, baseloc)
+        nobs = len(hdulists)
 
-        if i_lim is None:
-            i_lim = len(objids)
+        loglams = [h[1].data['loglam'] for h in hdulists]
+        fluxs = [h[1].data['flux'] for h in hdulists]
+        ivars = [h[1].data['ivar'] for h in hdulists]
+        masks = [(h[1].data['and_mask'] > 0) for h in hdulists]
 
-        mults_dict = {objids[i]:
-                      t.Table(objs_dupl['plate', 'mjd', 'fiberid'].groups[i])
-                      for i in range(i_lim - 1)}
+        # construct full wavelength grid
+        logl0 = min(list(map(min, loglams)))
+        loglf = max(list(map(max, loglams)))
+        logl_full = np.arange(logl0, loglf + dlogl, step=dlogl)
+        nl = len(logl_full)
+        i0 = list(map(lambda x: np.argmin(np.abs(x[0] - logl_full)), loglams))
 
-        SB_r_mean = np.mean(obs['SB_r']) * 1.0e-9 * m.Mgy / (u.arcsec)**2.
+        ivars_regr = np.zeros((nobs, nl))
+        fluxs_regr = np.zeros((nobs, nl))
+        masks_regr = np.zeros((nobs, nl))
 
-        return mults_dict, SB_r_mean
+        # put all objects onto same grid
+        for i, i0_ in enumerate(i0):
+            nl_ = len(fluxs[i])
+            fluxs[i, i0_:i0_ + nl_] = fluxs[i]
+            ivars[i, i0_:i0_ + nl_] = ivars[i]
+            masks[i, i0_:i0_ + nl_] = masks[i]
 
-    @staticmethod
-    def download_obj_specs(tab):
-        '''
-        for all objects in a `mults`-style dict, download their FITS spectra
-        '''
+        # propagate masks into ivars
+        ivars *= ~masks
 
-        def make_remote_fname(row):
-            return '{0}/spec-{0}-{1}-{2:04d}.fits'.format(*row)
-        remote_fnames = list(map(make_remote_fname, tab))
+        # combine fluxs into diffs
+        pairs = comb(range(nobs), 2)
+        diffs = np.row_stack([fluxs[i1, ...] - fluxs[i2, ...]
+                              for (i1, i2) in pairs])
 
-        def make_local_fname(row):
-            return os.path.join(
-                boss_calib_dir, 'spec-{0}-{1}-{2:04d}.fits'.format(*row))
-        local_fnames = list(map(make_local_fname, tab))
+        # combine ivars into weights
+        pairs = comb(range(nobs), 2)
+        ws = 1. / np.row_stack([1. / (1. / ivars[i1, ...] + 1. / ivars[i2, ...])
+                                for i1, i2 in pairs])
 
-        success = [False, ] * len(remote_fnames)
-        for i, (fname_r, fname_l) in enumerate(zip(remote_fnames,
-                                                   local_fnames)):
-            # if file has already been downloaded, move on
-            if os.path.isfile(fname_l):
-                success[i] = True
-                continue
+        return logl_full, diffs, ws
 
-            # if not, retrieve it over rsync!
-            print('Downloading remote file:', fname_r)
-            q = 'rsync -raz --password-file={0} rsync://sdss@{1} {2}'.format(
-                mangarc.password_file,  # SAS password
-                os.path.join(  # what file we're looking for
-                    m.base_url,
-                    'ebosswork/eboss/spectro/redux/v5_9_0/spectra/lite',
-                    fname_r),
-                boss_calib_dir)  # where to put it
-            # print(q)
-            s_ = os.system(q)  # os.system() returns 0 on success
-            if s_ == 0:
-                success[i] = True
-            elif s_ == 2:
-                raise KeyboardInterrupt
 
-        return boss_calib_dir, local_fnames, success
+def load_BOSS_obs(row, baseloc):
+    plate, mjd, fiberid = row['PLATE'], row['MJD'], row['FIBERID']
+    fname = '{0}/spec-{0}-{1}-{2:04}.fits'.format(plate, mjd, fiberid)
+    fname_full = os.path.join(baseloc, fname)
+    return fits.open(fname_full)
 
-    @staticmethod
-    def load_obj_spec(base_dir, fnames, success, data_name,
-                      lam_ix0s=None, nspec=None):
-        '''
-        for all files in a list, load and return an array of fluxes
-        '''
+def load_BOSS_obj(tab, baseloc):
+    return [load_BOSS_obs(row, baseloc) for row in tab]
 
-        # handle cacse that we want everything
-        if (lam_ix0s is None) or (nspec is None):
-            data = [fits.open(f)['COADD'].data[data_name]
-                    for f, s in zip(fnames, success) if s]
-        else:
-            try:
-                data = [fits.open(f)['COADD'].data[data_name][i0: i0 + nspec]
-                        for f, s, i0 in zip(fnames, success, lam_ix0s) if s]
-            # handle cases where wavelength solution is outside bounds
-            # shouldn't just throw out individual spectra, since that
-            # could list bring down to length-one and mess up statistics
-            except IndexError:
-                return None
-            # if things have different lengths
-            if True in list(map(lambda x: len(x) != nspec, data)):
-                return None
+def gen_BOSS_fname(row):
+    plate, mjd, fiberid = row['PLATE'], row['MJD'], row['FIBERID']
+    return '{0}/spec-{0}-{1}-{2:04}.fits'.format(plate, mjd, fiberid)
 
-        return data
+def put_on_grid(logl_full, logl_sub, A):
+    '''
+    use padding to place several arrays (elements of tuple A)
+        onto uniform grid
+    '''
 
-    @staticmethod
-    def load_zeronormed_obj_spec(base_dir, fnames, success,
-                                 lllim, nspec, i):
-        loglam = Cov_Obs.load_obj_spec(
-            base_dir, fnames, success, data_name='loglam')
-        # figure out where to start and end
-        lllim_log = np.log10(lllim)
-        lam_ix0s = [np.argmin((logl - lllim_log)**2.) for logl in loglam]
-
-        flux = Cov_Obs.load_obj_spec(
-            base_dir, fnames, success, data_name='flux', lam_ix0s=lam_ix0s,
-            nspec=nspec)
-        ivar = Cov_Obs.load_obj_spec(
-            base_dir, fnames, success, data_name='ivar', lam_ix0s=lam_ix0s,
-            nspec=nspec)
-
-        if (flux is None) or (ivar is None):
-            return np.nan * np.ones(nspec)
-
-        flux = np.row_stack(flux)
-        ivar = np.row_stack(ivar)
-        ivar = np.maximum(ivar, eps)
-        normed = flux - np.average(flux, weights=ivar, axis=0)
-
-        return np.concatenate([normed, ivar], axis=0)
+    logl0 = logl_sub[0]
+    ix0 = np.argmin(np.abs(logl_full - logl0))
+    lpad = ix0
+    rpad = len(logl_full) - (ix0 + len(logl_sub))
+    A_new = tuple(np.pad(
+                      A_, pad_width=[[0, 0], [lpad, rpad]], mode='constant',
+                      constant_values=[[0., 0.], [0., 0.]]) for A_ in A)
+    return A_new
 
 
 if __name__ == '__main__':
-    '''
+    #'''
     spAll_loc = os.path.join(mangarc.zpace_sdss_data_loc,
                              'boss', 'spAll-v5_9_0.fits')
     spAll = fits.open(spAll_loc, memmap=True)
     Cov_boss = Cov_Obs.from_spAll(spAll=spAll)
     Cov_boss.write_fits('boss_Kspec.fits')
-    '''
+    #'''
     # =====
-
+    '''
     Cov_manga = Cov_Obs.from_MaNGA_reobs(n=None)
     Cov_manga.write_fits('manga_Kspec.fits')
     Cov_manga.make_im(kind='manga')
+    '''
