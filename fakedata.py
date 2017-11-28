f60290551819c525c5fb04ae1b308e90ecf74365
zpace <zpace21@gmail.com>
2017-11-28 17:43:22 -0600

    noise samples from instrumental covariance

--- fakedata.py
+++ fakedata.py
@@ -5,6 +5,7 @@ from astropy.io import fits
 from astropy import units as u, constants as c, table as t
 
 from scipy.interpolate import interp1d
+from scipy.signal import medfilt
 
 import os, sys
 from copy import copy
@@ -26,20 +27,31 @@ if mangarc.tools_loc not in sys.path:
 # personal
 import manga_tools as m
 
+def noisify_cov(self, cov, mapshape, i0, nl):
+    assert cov is not None, 'covariance object not passed'
+
+    cov_noise = np.random.multivariate_normalrvs(
+        mean=np.zeros_like(np.diag(cov.cov)),
+        cov=cov.cov, size=mapshape)
+    cov_noise = np.moveaxis(cov_noise, [0, 1, 2], [2, 0, 1])
+    cov_noise = cov_noise[i0:i0 + nl, :, :]
+    rms = np.sqrt(np.mean(cov_noise, axis=0))
+    return cov_noise, rms
 
 class FakeData(object):
     '''
     Make a fake IFU and fake DAP stuff
     '''
     def __init__(self, l_model, s_model, meta_model,
-                 row, drp_base, dap_base, plateifu_base):
+                 row, drp_base, dap_base, plateifu_base, model_ix,
+                 noisify_method='cov', Kspec_obs=None):
+
+        self.model_ix = model_ix
 
         # find SNR of each pixel in cube (used to scale noise later)
-        drp_snr = np.repeat(
-            np.median(
-                drp_base['FLUX'].data * np.sqrt(drp_base['IVAR'].data),
-                axis=0)[None, :, :],
-            drp_base['FLUX'].data.shape[0], axis=0)
+        drp_snr = medfilt(drp_base['FLUX'].data * np.sqrt(drp_base['IVAR'].data),
+                          [15, 1, 1])
+        spax_rms = np.mean(drp_snr, axis=0)
 
         # redshift the model spectrum
         z_cosm = row['nsa_zdist']
@@ -72,21 +84,36 @@ class FakeData(object):
                                       z_in=0., z_out=z_out)
         logl_m_z = np.log10(l_m_z)
 
-        # interpolate redshifted model cube
-        logl_f = np.log10(drp_base['WAVE'].data)
-        s_m_z_c = np.zeros(cubeshape)
-
         # redden model cube according to galactic E(B-V)
         s_m_z = ut.extinction_atten(
             l=l_m_z * u.AA, f=s_m_z, EBV=drp_base[0].header['EBVGAL'])
 
-        rimg = drp_base['RIMG'].data
+        # interpolate redshifted model cube
+        logl_f = np.log10(drp_base['WAVE'].data)
+        s_m_z_c = np.zeros(cubeshape)
 
         for ind in np.ndindex(mapshape):
             newspec = self.resample_spaxel(
                 logl_in=logl_m_z[:, ind[0], ind[1]],
                 flam_in=s_m_z[:, ind[0], ind[1]], logl_out=logl_f)
             s_m_z_c[:, ind[0], ind[1]] = newspec
+
+        # add error vector based on SNR calculated above
+        # SNR = F sqrt(I) ===> (SNR / F)^2 = I
+
+        if noisify_method == 'cov':
+            # assume cosmological redshift
+            # key onto **final** value in model wavelengths
+            ifinal = np.argmin(np.abs(l_model[-1] * (1. + z_cosm) - cov.l))
+            i0 = ifinal - nl
+            noise, noise_rms = noisify_cov(
+                Kspec_obs, mapshape=mapshape, i0=i0, nl=nl)
+        elif noisify_method == 'ivar':
+            noise = np.random.randn(*cubeshape) / drp_snr
+            noise_rms = np.sqrt(np.mean(noise**2., axis=0))
+
+        noise_rmsscaled = noise * (spax_rms / noise_rms)[None, :, :]
+        fakecube = s_m_z_c * (1. + noise_rmsscaled)
 
         # normalize everything to have the same observed-frame r-band flux
         u_flam = 1.0e-17 * (u.erg / (u.s * u.cm**2 * u.AA))
@@ -98,22 +125,18 @@ class FakeData(object):
         r = 10.**(-0.4 * (m_r_drp - m_r_mzc))
         # occasionally this produces inf or nan for some reason,
         # so just take care of that quickly
-        r[~np.isfinite(r)] = 1.
-        s_m_z_c *= r[None, ...]
+        r[np.log10(r) > 4.] = 4.
+        r[np.log10(r) < -4] = -4.
+        fakecube *= r[None, ...]
 
-        # add error vector based on SNR calculated above
-        err = np.random.randn(*cubeshape)
-        ivar_obs = ((drp_snr / s_m_z_c)**2.).clip(min=1.0e-6)
-        fakecube = s_m_z_c + (err / np.sqrt(ivar_obs))
-        fakecube[ivar_obs == 0] = (100. * r[None, ...] * \
-                                   np.random.randn(*cubeshape))[ivar_obs == 0.]
+        ivar_cube = (noise_rmsscaled * r)**2.
 
         # mask where the native datacube has no signal
-        nosignal = (drp_base['RIMG'].data == 0.)[None, ...]
+        rimg = drp_base['RIMG'].data
+        nosignal = (rimg == 0.)[None, ...]
         nosignal_cube = np.broadcast_to(nosignal, fakecube.shape)
         fakecube[nosignal_cube] = 0.
-        ivar_obs[s_m_z_c == 0.] = 0
-        drp_base['IVAR'].data = ivar_obs
+        ivar_obs[s_m_z_c == 0.] = 0.
 
         # mask where there's bad velocity info
         badvel = m.mask_from_maskbits(
@@ -126,6 +149,7 @@ class FakeData(object):
         self.dap_base = dap_base
         self.drp_base = drp_base
         self.fluxcube = fakecube
+        self.fluxcube_ivar = ivar_obs
         self.row = row
         self.metadata = meta_model
 
@@ -175,7 +199,8 @@ class FakeData(object):
 
         return cls(l_model=models_lam, s_model=model_spec,
                    meta_model=model_meta, row=row, plateifu_base=plateifu_base,
-                   drp_base=drp_base, dap_base=dap_base)
+                   drp_base=drp_base, dap_base=dap_base, model_ix=i,
+                   Kspec_obs=pca.Kspec_obs)
 
     def resample_spaxel(self, logl_in, flam_in, logl_out):
         '''
@@ -202,12 +227,17 @@ class FakeData(object):
         new_drp_cube = fits.HDUList([hdu for hdu in self.drp_base])
         new_drp_cube['FLUX'].data = self.fluxcube
 
+        new_drp_cube['IVAR'].data = self.fluxcube_ivar
+
         new_dap_cube = fits.HDUList([hdu for hdu in self.dap_base])
         sig_a = np.sqrt(
             self.metadata['sigma']**2. * \
                 np.ones_like(new_dap_cube['STELLAR_SIGMA'].data) + \
             new_dap_cube['STELLAR_SIGMACORR'].data**2.)
         new_dap_cube['STELLAR_SIGMA'].data = sig_a
+
+        new_drp_cube[0].header['MODEL0'] = self.model_ix
+        new_dap_cube[0].header['MODEL0'] = self.model_ix
 
         truth_tab = t.Table(
             rows=[self.metadata],
