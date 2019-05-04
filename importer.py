dcdf3d9c166d07258eaf859c6c3e662b4067c460
Zach Pace <zpace@astro.wisc.edu>
2019-05-04 10:41:32 -0600

    importer now uses environment variables

--- importer.py
+++ importer.py
@@ -1,19 +1,11 @@
 import os, sys, matplotlib
 
-# add manga RC location to path, and import config
-if os.environ['MANGA_CONFIG_LOC'] not in sys.path:
-    sys.path.append(os.environ['MANGA_CONFIG_LOC'])
-
-import mangarc
-
-if mangarc.tools_loc not in sys.path:
-    sys.path.append(mangarc.tools_loc)
-
 mpl_v = 'MPL-8'
 daptype = 'SPX-MILESHC-MILESHC'
-csp_basedir = '/usr/data/minhas2/zpace/CSPs/CSPs_CKC14_MaNGA_20190215-1'
-manga_results_basedir = '/usr/data/minhas2/zpace/CSPs/CSPs_CKC14_MaNGA_20190215-1'
-mocks_results_basedir = '/usr/data/minhas2/zpace/CSPs/CSPs_CKC14_MaNGA_20190215-1/fakedata'
+csp_basedir = os.environ['STELLARMASS_PCA_CSPBASE']
+manga_results_basedir = os.environ['STELLARMASS_PCA_RESULTSDIR']
+mocks_results_basedir = os.path.join(
+    os.environ['STELLARMASS_PCA_RESULTSDIR'], 'mocks')
 
 from astropy.cosmology import WMAP9
 cosmo = WMAP9
