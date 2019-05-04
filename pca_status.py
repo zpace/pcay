7f1d1bc8650faf3abf813811da69a4895e19f2a0
zpace <zpace21@gmail.com>
2019-05-04 13:02:43 -0500

    make sure status files go to the right place

--- pca_status.py
+++ pca_status.py
@@ -4,7 +4,7 @@ import os
 
 def log_file_exists(plateifu):
     status_file_dir = os.path.join(
-        os.environ['STELLARMASS_PCA_RESULTSDIR'],
+        os.environ['STELLARMASS_PCA_RESULTSDIR'], 'results',
         plateifu)
 
     if os.path.exists(os.path.join(status_file_dir, '{}.log'.format(plateifu))):
@@ -14,7 +14,7 @@ def write_log_file(plateifu, msg):
     '''write a log file
     '''
     status_file_dir = os.path.join(
-        os.environ['STELLARMASS_PCA_RESULTSDIR'],
+        os.environ['STELLARMASS_PCA_RESULTSDIR'], 'results',
         plateifu)
 
     if not os.path.exists(status_file_dir):
