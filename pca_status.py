d7779e98cbe7e6ed367a22f219045109f0af31b5
zpace <zpace21@gmail.com>
2019-05-08 16:31:54 -0500

    add summarize remaining

--- pca_status.py
+++ pca_status.py
@@ -22,4 +22,10 @@ def write_log_file(plateifu, msg):
     with open(os.path.join(status_file_dir, '{}.log'.format(plateifu)), 'w') as logf:
         logf.write(msg)
 
-
+def summary_remaining(drpall, group_col='ifudesignsize'): 
+    remaining = drpall[ 
+        ~np.array(list(map(log_file_exists, drpall['plateifu'])))] 
+    ifusize_grps = remaining.group_by(group_col) 
+    print('remaining galaxies by {}'.format(group_col)) 
+    for k, g in zip(ifusize_grps.groups.keys, ifusize_grps.groups): 
+        print(k[group_col], ':', len(g))
